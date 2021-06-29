e101bcfc61134ea26f2d50c5f4019c392ae0e30e
Your Nameasrunclearclear <you@example.com>
2021-06-29 21:58:21 +0900

    commit

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ showBtn.forEach(function (btn) {
   const nextElment = parent.nextElementSibling;
   btn.addEventListener('click', function () {
     parent.classList.add('hide');
-
     nextElment.classList.add('show-section');
+    const prev = nextElment.previousElementSibling;
+    console.log(prev);
+    if (nextElment.classList.contains('show-section')) {
+      prev.classList.remove('hide');
+      prev.classList.remove('show-section');
+    }
   });
 });
+
+const date = document.getElementById('date');
+const year = new Date().getFullYear();
+
+date.textContent = year;
+console.log(date);
